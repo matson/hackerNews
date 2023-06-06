4c612a10969fb916b1df136a982406cd2f7b5e8d
Madison <madison.k.adams@gmail.com>
2023-06-06 14:50:26 -0400

    starter code

---
+++ hackerNews/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  hackerNews
+//
+//  Created by Tracy Adams on 6/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+                
+            }.navigationBarTitle("Hacker News")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
---
+++ hackerNews/hackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsApp.swift
+//  hackerNews
+//
+//  Created by Tracy Adams on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
